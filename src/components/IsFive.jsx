1703d67aa7cb0d540c14764a988004ba6820b945
allexib <ooomegasib@gmail.com>
2021-05-17 13:18:42 +0400

    prevProps, nevProps, rerender

--- src/components/IsFive.jsx
+++ src/components/IsFive.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 
 let renderCount = 0;
 
-export default React.memo(function  IsFive({value}) {
-    console.warn(`isFive render: ${++renderCount}`);
+export default React.memo(function IsFive({value}) {
+        console.warn(`isFive render: ${++renderCount}`);
 
-    const getResult = React.useMemo(() => {
-        let i = 0;
-        while (i < 60000000) i++;
-        return value === 5 ? ` Это пять :D` : `Это не пять :(`;
-    }, [value]);
+        const getResult = React.useMemo(() => {
+            let i = 0;
+            while (i < 60000000) i++;
+            return value === 5 ? ` Это пять :D` : `Это не пять :(`;
+        }, [value]);
 
-    return (
-        <h3>{getResult}</h3>
-    )
-});
+        return (
+            <h3>{getResult}</h3>
+        )
+    },
+    (prevProps, nextProps) => {
+        if (nextProps.value === 5) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+);
