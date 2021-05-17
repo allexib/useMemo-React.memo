c0c15c534d07df0a3aaa37c6e7cadd4249b9d1b2
allexib <ooomegasib@gmail.com>
2021-05-17 13:05:14 +0400

    useMemo IsFive, getResult

--- src/components/IsFive.jsx
+++ src/components/IsFive.jsx
@@ -5,13 +5,13 @@ let renderCount = 0;
 export default function  IsFive({value}) {
     console.warn(`isFive render: ${++renderCount}`);
 
-    const getResult = () => {
+    const getResult = React.useMemo(() => {
         let i = 0;
         while (i < 60000000) i++;
         return value === 5 ? ` Это пять :D` : `Это не пять :(`;
-    };
+    }, [value]);
 
     return (
-        <h3>{getResult()}</h3>
+        <h3>{getResult}</h3>
     )
 };
